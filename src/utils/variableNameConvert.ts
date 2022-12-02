490de3af71a62f60c4568dc9438cd79b2b84a233
ray_tsai <ray.tsai@howdesign.com.tw>
2022-12-02 14:11:42 +0800

    feat: update variableNameConvert

--- src/utils/variableNameConvert.ts
+++ src/utils/variableNameConvert.ts
@@ -1,30 +1,104 @@
 import { isArray, isPlainObject } from 'lodash-es';
 
 // 首字母大寫
-export const firstLetterUpperCase = (str: string): string => {
+export const firstLetterToUpperCase = (str: string): string => {
   return str[0].toUpperCase() + str.slice(1);
 };
 
-// 首字母小寫
-// export const fistLetterLowerCase = (str: string): string => {
-//   return str[0].toLowerCase() + str.slice(1);
-// };
+export const firstLetterToLowerCase = (str: string): string => {
+  return str[0].toLowerCase() + str.slice(1);
+};
 
+// kebab-case 轉為其他格式
 // kebab-case to camelCase
 export const kebabCaseToCamelCase = (str: string) => {
   // 驗證格式對不對
-  const regExp = /[^A-Za-z-]/;
-  const isValidate = regExp.test(str);
-  if (isValidate) {
-    throw new Error('格式錯誤');
-  }
+  // const regExp = /[^A-Za-z-]/;
+  // const isValidate = regExp.test(str);
+  // if (isValidate) {
+  //   throw new Error('格式錯誤');
+  // }
 
-  return str.replace(/-\w/g, matchValue => matchValue.slice(1).toUpperCase());
+  return str.replace(/-\w/g, (matchValue) => {
+    return matchValue.slice(1).toUpperCase();
+  });
 };
 
-export const camelToSnakeCase = (str: string): string => {
-  return str.replace(/[A-Z]/g, (letter: string) => `_${letter.toLowerCase()}`);
+// kebab-case to PascalCase
+export const kebabCaseToPascalCase = (str: string) => {
+  const words = str.split('-');
+
+  return words.reduce((str: string, w: string) => {
+    str += firstLetterToUpperCase(w);
+    return str;
+  }, '');
 };
+
+// camelCase to SnakeCase
+export const kebabCaseToSnakeCase = (str: string) => {
+  return str.replace(/-/g, '_');
+};
+
+// snake_case 轉為其他格式
+// snake_case to camelCase
+export const SnakeCaseToCamelCase = (str: string) => {
+  return str.replace(/_\w/g, (matchValue) => {
+    return matchValue.slice(1).toUpperCase();
+  });
+};
+
+// snake_case to PascalCase
+export const SnakeCaseToPascalCase = (str: string) => {
+  const words = str.split('_');
+
+  return words.reduce((str: string, w: string) => {
+    str += firstLetterToUpperCase(w);
+    return str;
+  }, '');
+};
+
+// snake_case to kebab-case
+export const SnakeCaseToKebabCase = (str: string) => {
+  return str.replace(/_/g, '-');
+};
+
+// camelCase 轉為其他格式
+// camelCase to kebab-case
+export const camelCaseToKebabCase = (str: string) => {
+  return str.replace(/[A-Z]/g, (matchValue: string) => `-${matchValue.toLowerCase()}`);
+};
+
+// camelCase to snake_case
+export const camelCaseToSnakeCase = (str: string) => {
+  return str.replace(/[A-Z]/g, (matchValue: string) => `_${matchValue.toLowerCase()}`);
+};
+
+// camelCase to PascalCase
+export const camelCaseToPascalCase = (str: string): string => {
+  return firstLetterToUpperCase(str);
+};
+
+// PascalCase 轉為其他格式
+
+export const pascalCaseToKebabCase = (str: string) => {
+  const lowerLetter = firstLetterToLowerCase(str);
+  return lowerLetter.replace(/[A-Z]/g, (matchValue: string) => `-${matchValue.toLowerCase()}`);
+};
+
+// PascalCase to snake_case
+export const pascalCaseToSnakeCase = (str: string) => {
+  const lowerLetter = firstLetterToLowerCase(str);
+  return lowerLetter.replace(/[A-Z]/g, (matchValue: string) => `_${matchValue.toLowerCase()}`);
+};
+
+// PascalCase to CamelCase
+export const pascalCaseToCamelCase = (str: string): string => {
+  return firstLetterToLowerCase(str);
+};
+
+// export const camelCaseToSnakeCase = (str: string): string => {
+//   return firstLetterToUpperCase(str);
+// };
 
 const toSnakeCasePayload = (paylaod: { [key: string]: any }) => {
   const keys: string[] = Object.keys(paylaod);
--- src/views/common/VariableNameConvert.vue
+++ src/views/common/VariableNameConvert.vue
@@ -5,15 +5,47 @@
 </template>
 
 <script setup lang="ts">
-import { firstLetterUpperCase, kebabCaseToCamelCase } from '@/utils/variableNameConvert';
+import {
+  SnakeCaseToCamelCase,
+  SnakeCaseToKebabCase,
+  SnakeCaseToPascalCase,
+  camelCaseToKebabCase,
+  camelCaseToPascalCase,
+  camelCaseToSnakeCase,
+  firstLetterToUpperCase,
+  kebabCaseToCamelCase,
+  kebabCaseToPascalCase,
+  kebabCaseToSnakeCase,
+  pascalCaseToCamelCase,
+  pascalCaseToKebabCase,
+  pascalCaseToSnakeCase,
+} from '@/utils/variableNameConvert';
 
 const test1 = 'test';
 const test2 = 'TEST';
 const test3 = 're-multi-select';
+const test4 = 're_multi_select';
+const test5 = 'reMultiSelect';
+const test6 = 'ReMultiSelect';
 
-console.log('firstLetterUpperCase...test1', firstLetterUpperCase(test1));
-console.log('firstLetterUpperCase...test2', firstLetterUpperCase(test2));
-console.log('kebabCaseToCamelCase...test3', kebabCaseToCamelCase(test3));
+console.log(`firstLetterToUpperCase|${test1} --> `, firstLetterToUpperCase(test1));
+console.log(`firstLetterToUpperCase|${test2} --> `, firstLetterToUpperCase(test2));
+console.log('---');
+console.log(`kebabCaseToCamelCase|${test3} --> `, kebabCaseToCamelCase(test3));
+console.log(`kebabCaseToPascalCase|${test3} --> `, kebabCaseToPascalCase(test3));
+console.log(`kebabCaseToSnakeCase|${test3} --> `, kebabCaseToSnakeCase(test3));
+console.log('---');
+console.log(`SnakeCaseToCamelCase|${test4} --> `, SnakeCaseToCamelCase(test4));
+console.log(`SnakeCaseToPascalCase|${test4} --> `, SnakeCaseToPascalCase(test4));
+console.log(`SnakeCaseToKebabCase|${test4} --> `, SnakeCaseToKebabCase(test4));
+console.log('---');
+console.log(`camelCaseToKebabCase|${test5} --> `, camelCaseToKebabCase(test5));
+console.log(`camelCaseToSnakeCase|${test5} --> `, camelCaseToSnakeCase(test5));
+console.log(`camelCaseToPascalCase|${test5} --> `, camelCaseToPascalCase(test5));
+console.log('---');
+console.log(`pascalCaseToKebabCase|${test6} --> `, pascalCaseToKebabCase(test6));
+console.log(`pascalCaseToSnakeCase|${test6} --> `, pascalCaseToSnakeCase(test6));
+console.log(`pascalCaseToCamelCase|${test6} --> `, pascalCaseToCamelCase(test6));
 
 // console.log('fistLetterLowerCase...', fistLetterLowerCase(fruitOptions));
 
