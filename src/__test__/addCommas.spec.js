c2f4f5c99e95e6d40af9c031853ab426d7ceea38
tsai_ray <silentice1534@gmail.com>
2022-12-03 23:13:10 +0800

    feat: addComma spec

--- src/__test__/addCommas.spec.js
+++ src/__test__/addCommas.spec.js
@@ -1,122 +1,146 @@
 import { describe, expect, it } from 'vitest';
 import addCommas from '@/utils/addCommas';
 
-describe('addCommas 正數1', () => {
-  it('1 should be \'1\'', () => {
-    expect(addCommas(1)).toBe('1');
+const postiveTestCase = [
+  {
+    testDesc: '0.1 應為 \'0.1\'',
+    input: 0.1,
+    output: '0.1',
+  },
+  {
+    testDesc: '0.01 應為 \'0.01\'',
+    input: 0.01,
+    output: '0.01',
+  },
+  {
+    testDesc: '0.001 應為 \'0.001\'',
+    input: 0.001,
+    output: '0.001',
+  },
+  {
+    testDesc: '0.0001 應為 \'0.0001\'',
+    input: 0.0001,
+    output: '0.0001',
+  },
+  {
+    testDesc: '0.12345678 應為 \'0.12345678\'',
+    input: 0.12345678,
+    output: '0.12345678',
+  },
+];
+
+const postiveDecimalTestCase = [
+  {
+    testDesc: '-1 應為 \'-1\'',
+    input: -1,
+    output: '-1',
+  },
+  {
+    testDesc: '-10 應為 \'-10\'',
+    input: -10,
+    output: '-10',
+  },
+  {
+    testDesc: '-100 應為 \'-100\'',
+    input: -100,
+    output: '-100',
+  },
+  {
+    testDesc: '-1000 應為 \'-1000\'',
+    input: -1000,
+    output: '-1,000',
+  },
+  {
+    testDesc: '-12345678 應為 \'-12,345,678\'',
+    input: -12345678,
+    output: '-12,345,678',
+  },
+];
+
+const negativeTestCase = [
+  {
+    testDesc: '1 應為 \'1\'',
+    input: 1,
+    output: '1',
+  },
+  {
+    testDesc: '10 應為 \'10\'',
+    input: 10,
+    output: '10',
+  },
+  {
+    testDesc: '100 應為 \'100\'',
+    input: 100,
+    output: '100',
+  },
+  {
+    testDesc: '1000 應為 \'1000\'',
+    input: 1000,
+    output: '1,000',
+  },
+  {
+    testDesc: '12345678 應為 \'12,345,678\'',
+    input: 12345678,
+    output: '12,345,678',
+  },
+];
+
+const negativeDecimalTestCase = [
+  {
+    testDesc: '-0.1 應為 \'-0.1\'',
+    input: -0.1,
+    output: '-0.1',
+  },
+  {
+    testDesc: '-0.01 應為 \'-0.01\'',
+    input: -0.01,
+    output: '-0.01',
+  },
+  {
+    testDesc: '-0.001 應為 \'-0.001\'',
+    input: -0.001,
+    output: '-0.001',
+  },
+  {
+    testDesc: '-0.0001 應為 \'-0.0001\'',
+    input: -0.0001,
+    output: '-0.0001',
+  },
+  {
+    testDesc: '-0.12345678 應為 \'-0.12345678\'',
+    input: -0.12345678,
+    output: '-0.12345678',
+  },
+];
+
+describe('addCommas 正數', () => {
+  postiveTestCase.forEach((params) => {
+    it(params.testDesc, () => {
+      expect(addCommas(params.input)).toBe(params.output);
+    });
   });
 });
 
-describe('addCommas 正數2', () => {
-  it('10 should be \'10\'', () => {
-    expect(addCommas(10)).toBe('10');
+describe('addCommas 正小數', () => {
+  postiveDecimalTestCase.forEach((params) => {
+    it(params.testDesc, () => {
+      expect(addCommas(params.input)).toBe(params.output);
+    });
   });
 });
 
-describe('addCommas 正數3', () => {
-  it('100 should be \'100\'', () => {
-    expect(addCommas(100)).toBe('100');
+describe('addCommas 負數', () => {
+  negativeTestCase.forEach((params) => {
+    it(params.testDesc, () => {
+      expect(addCommas(params.input)).toBe(params.output);
+    });
   });
 });
 
-describe('addCommas 正數4', () => {
-  it('1000 should be \'1,000\'', () => {
-    expect(addCommas(1000)).toBe('1,000');
-  });
-});
-
-describe('addCommas 正數5', () => {
-  it('12345678 should be \'12,345,678\'', () => {
-    expect(addCommas(12345678)).toBe('12,345,678');
-  });
-});
-
-describe('addCommas 正小數1', () => {
-  it('0.1 should be \'0.1\'', () => {
-    expect(addCommas(0.1)).toBe('0.1');
-  });
-});
-
-describe('addCommas 正小數2', () => {
-  it('0.01 should be \'0.01\'', () => {
-    expect(addCommas(0.01)).toBe('0.01');
-  });
-});
-
-describe('addCommas 正小數3', () => {
-  it('0.001 should be \'0.001\'', () => {
-    expect(addCommas(0.001)).toBe('0.001');
-  });
-});
-
-describe('addCommas 正小數4', () => {
-  it('0.0001 should be \'0.0001\'', () => {
-    expect(addCommas(0.0001)).toBe('0.0001');
-  });
-});
-
-describe('addCommas 正小數5', () => {
-  it('0.12345678 should be \'0.12345678\'', () => {
-    expect(addCommas(0.12345678)).toBe('0.12345678');
-  });
-});
-
-describe('addCommas 負數1', () => {
-  it('-1 should be \'1\'', () => {
-    expect(addCommas(-1)).toBe('-1');
-  });
-});
-
-describe('addCommas 負數2', () => {
-  it('-10 should be \'-10\'', () => {
-    expect(addCommas(-10)).toBe('-10');
-  });
-});
-
-describe('addCommas 負數3', () => {
-  it('-100 should be \'-100\'', () => {
-    expect(addCommas(-100)).toBe('-100');
-  });
-});
-
-describe('addCommas 負數4', () => {
-  it('-1000 should be \'-1,000\'', () => {
-    expect(addCommas(-1000)).toBe('-1,000');
-  });
-});
-
-describe('addCommas 負數5', () => {
-  it('-12345678 should be \'-12,345,678\'', () => {
-    expect(addCommas(-12345678)).toBe('-12,345,678');
-  });
-});
-
-describe('addCommas 負小數1', () => {
-  it('-0.1 should be \'0.1\'', () => {
-    expect(addCommas(0.1)).toBe('0.1');
-  });
-});
-
-describe('addCommas 負小數2', () => {
-  it('-0.01 should be \'0.01\'', () => {
-    expect(addCommas(0.01)).toBe('0.01');
-  });
-});
-
-describe('addCommas 負小數3', () => {
-  it('-0.001 should be \'0.001\'', () => {
-    expect(addCommas(0.001)).toBe('0.001');
-  });
-});
-
-describe('addCommas 負小數4', () => {
-  it('-0.0001 should be \'0.0001\'', () => {
-    expect(addCommas(0.0001)).toBe('0.0001');
-  });
-});
-
-describe('addCommas 負小數5', () => {
-  it('-0.12345678 should be \'-0.12345678\'', () => {
-    expect(addCommas(-0.12345678)).toBe('-0.12345678');
+describe('addCommas 負小數', () => {
+  negativeDecimalTestCase.forEach((params) => {
+    it(params.testDesc, () => {
+      expect(addCommas(params.input)).toBe(params.output);
+    });
   });
 });
